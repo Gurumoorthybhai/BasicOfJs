04ca24b30a76bddb6419a110e8aa264e8d3c4b26
Gurumoorthy M <gurunaveen22@gmail.com>
2023-11-11 21:39:47 +0530

    polyfill for include

---
+++ Polyfills/main.js
@@ -0,0 +1,15 @@
+// polyfill for Array.prototype.includes()
+
+if(!Array.prototype.includess) {
+Array.prototype.includess = function(searchElement) {
+for (var i = 0 ; i < this.length ; i++ ) {
+    if (this[i] === searchElement) {
+        return true;
+    }
+}
+return false;
+};
+}
+
+let arr = [1,2,3,4,5];
+console.log(arr.includess(10));
