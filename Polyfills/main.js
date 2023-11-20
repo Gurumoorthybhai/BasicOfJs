8e264a6b9197d8f817bffc9af23e8381b7e12cc9
Gurumoorthy M <gurunaveen22@gmail.com>
2023-11-20 07:44:56 +0530

    Polyfill of forEach

--- Polyfills/main.js
+++ Polyfills/main.js
@@ -1,15 +1,28 @@
 // polyfill for Array.prototype.includes()
 
-if(!Array.prototype.includess) {
-Array.prototype.includess = function(searchElement) {
-for (var i = 0 ; i < this.length ; i++ ) {
-    if (this[i] === searchElement) {
-        return true;
-    }
-}
-return false;
-};
-}
 
-let arr = [1,2,3,4,5];
-console.log(arr.includess(10));
+
+// if(!Array.prototype.includess) {
+// Array.prototype.includess = function(searchElement) {
+// for (var i = 0 ; i < this.length ; i++ ) {
+//     if (this[i] === searchElement) {
+//         return true;
+//     }
+// }
+// return false;
+// };
+// }
+
+// let arr = [1,2,3,4,5];
+// console.log(arr.includess(10));
+
+
+// polyfill for the foreach
+
+Object.defineProperty(Array.prototype, 'forEach', {
+    value: function(callback) {
+        const arr = [];
+
+        
+    }
+})
